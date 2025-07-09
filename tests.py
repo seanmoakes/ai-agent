6731864c544b784cd1377440759fc5ca1fd21cd1
Sean Moakes <mail@seanmoakes.com>
2025-07-09 12:30:12 +0100

    get_file_content

--- tests.py
+++ tests.py
@@ -1,22 +1,12 @@
-from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
 
 def test():
-    result = get_files_info("calculator", ".")
-    print("Result for current directory:")
+    result = get_file_content("calculator", "main.py")
     print(result)
-    print("")
-
-    result = get_files_info("calculator", "pkg")
-    print("Result for 'pkg' directory:")
+    result = get_file_content("calculator", "pkg/calculator.py")
     print(result)
-
-    result = get_files_info("calculator", "/bin")
-    print("Result for '/bin' directory:")
-    print(result)
-
-    result = get_files_info("calculator", "../")
-    print("Result for '../' directory:")
+    result = get_file_content("calculator", "/bin/cat")
     print(result)
 
 
