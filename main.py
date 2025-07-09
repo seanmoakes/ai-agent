50d45b94d62e929e4a665b81c70b0ca33f619ca8
Sean Moakes <mail@seanmoakes.com>
2025-07-09 15:55:39 +0100

    add file info function to prompt

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from functions.get_files_info import schema_get_files_info
+from config import system_prompt
 
 
 def main():
@@ -33,17 +35,29 @@ def main():
 
 
 def generate_content(client, messages, verbose):
-    system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+        ]
+    )
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
-        config=types.GenerateContentConfig(system_instruction=system_prompt),
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt
+        ),
     )
     if verbose:
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    print("Response:")
-    print(response.text)
+    if not response.function_calls:
+        print("Response:")
+        print(response.text)
+    else:
+        for function_call_part in response.function_calls:
+            print(
+                f"Calling function: {function_call_part.name}({function_call_part.args})"
+            )
 
 
 if __name__ == "__main__":
